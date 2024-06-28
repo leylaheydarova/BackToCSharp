abbb5fb613bbc37e4fd8a7917f3fe5b9dfea090a
leylaheydarova <lbadalzade@gmail.com>
2024-06-28 17:25:31 +0400

    Little sample

---
+++ CodeFirst.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using CodeFirst.App.context;
+using CodeFirst.App.Entities;
+
+var _context = new AppDbContext();
+var product = new Product()
+{
+    Name = "Product A",
+    Price = 4.50
+};
+
+_context.Produts.Add(product);
+_context.SaveChanges();
---
+++ CodeFirst.App/context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirst.App.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.App.context
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product>Produts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=CodeFirstDb;Integrated Security = true; TrustServerCertificate=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
